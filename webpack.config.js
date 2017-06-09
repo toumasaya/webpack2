a8ca2dd9a601218814f406fccf4e43bcb6f30e68
toumasaya <luffy.w.pirate@gmail.com>
2017-06-09 16:05:39 +0800

    Add sass-loader and node-sass

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/, use: ['style-loader', 'css-loader']
+        test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   }
