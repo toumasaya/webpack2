5005267da9de65bf40e79c00b096b38b5a57a071
toumasaya <luffy.w.pirate@gmail.com>
2017-06-10 09:33:07 +0800

    Add HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
@@ -25,19 +26,22 @@ module.exports = {
     }),
     new ExtractTextPlugin({
       filename: 'app.css',
-      disable: false,
+      disable: true,
       allChunks: true
     }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
   ],
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader'],
-          publicPath: './dist'
-        }),
+        // use: ExtractTextPlugin.extract({
+        //   fallback: 'style-loader',
+        //   use: ['css-loader', 'sass-loader'],
+        //   publicPath: './dist'
+        // }),
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.js$/,
@@ -54,6 +58,7 @@ module.exports = {
     contentBase: path.join(__dirname, "dist"), // 要跑 server 的目錄
     compress: true, // 用 gzip 壓縮檔案
     stats: "errors-only", // 只輸出錯誤訊息
-    open: true // 第一次啟動會自動打開瀏覽器
+    open: true, // 第一次啟動會自動打開瀏覽器
+    hot: true, // 啟動 HMR
   }
 }
